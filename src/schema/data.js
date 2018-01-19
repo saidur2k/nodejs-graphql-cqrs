881df5d11df2a2777507da2b6ddecc068eca8c22
Saidur Rahman <saidur2k@hotmail.com>
2018-01-19 21:24:27 +1100

    basic cqrs with graphql

---
+++ src/schema/data.js
@@ -0,0 +1,22 @@
+module.exports = {
+  data: [
+    {
+      title: 'Dance Party',
+      time: '2018-10-27T13:00:00Z',
+      host: 'Caramba',
+      location: 'The Beach'
+    },
+    {
+      title: 'Electro Club',
+      time: '2018-09-12T21:00:00Z',
+      host: 'Das Haus',
+      location: 'Bäsëmënt'
+    },
+    {
+      title: 'New Years',
+      time: '2018-01-01T00:00:00Z',
+      host: 'The Gregorians',
+      location: 'The World'
+    }
+  ]
+};
--- src/schema/index.js
+++ src/schema/index.js
@@ -1,17 +1,46 @@
 const { makeExecutableSchema } = require('graphql-tools');
 const gql = require('graphql-tag');
+const { GraphQLDateTime } = require('graphql-iso-date');
+
+const { data } = require('./data');
 
 const typeDefs = gql`
+  scalar DateTime
+
+  type Show {
+    title: String!
+    time: String!
+    host: String!
+    location: String!
+  }
+
   type Query {
-    helloWorld: String
+    allShows: [Show!]!
+  }
+
+  type Mutation {
+    addShow(
+      title: String!
+      time: DateTime!
+      host: String!
+      location: String!
+    ): Boolean
   }
 `;
 
+const fakeData = data;
 module.exports = makeExecutableSchema({
   typeDefs,
   resolvers: {
+    DateTime: GraphQLDateTime,
     Query: {
-      helloWorld: () => 'Hello World!'
+      allShows: () => fakeData
+    },
+    Mutation: {
+      addShow: (_, data) => {
+        fakeData.push(data);
+        return true;
+      }
     }
   }
 });
